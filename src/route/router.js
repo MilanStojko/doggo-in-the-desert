63798c8cbcaa7ba780fb6cbf70c01008559d2e22
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-09-20 14:37:58 +0200

    app created

---
+++ src/route/router.js
@@ -0,0 +1,10 @@
+const SCREENS = {
+  home: "/",
+  login: "/login",
+  signup: "/signup",
+  tutorial: "/tutorial",
+  game: "/game",
+  result: "/result",
+  leaderboard: "/leaderboard",
+};
+export default SCREENS;
