db2f831f14512b060663e0b41324db9bc589e552
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-20 17:39:10 +0200

    doggo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './app.scss';
 
-import InputBox from './components/ui/inputBox/InputBox';
 
 function App() {
   return (
     <div>
-
     </div>
   );
 }
---
+++ src/components/hook/doggo/Doggo.js
@@ -0,0 +1,10 @@
+import './doggo.scss'
+
+function Doggo(props) {
+  return (
+    <div className="dog">
+      <img className="doggo_img" src={require("../../../assets/images/spritesheet.png")} alt="dog" />
+    </div>
+  )
+}
+export default Doggo
