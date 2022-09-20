b8793a34587c1169b915faec88434bc32e6b3f30
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-20 17:26:47 +0200

    inputBox

---
+++ src/components/ui/inputBox/InputBox.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types"
+
+import './inputBox.scss'
+
+function InputBox(props) {
+  function change(e) {
+    if (!!props.callBackChange) {
+      props.callBackChange(e)
+    }
+  }
+
+  function focus(e) {
+    if (!!props.callBackChange) {
+      props.callBackFocus(e)
+    }
+  }
+
+  return (
+    <>
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        className={props.classCss}
+        style={props.objCss}
+        onChange={change}
+        onFocus={focus}
+      />
+    </>
+  )
+}
+
+InputBox.propTypes = {
+    type: PropTypes.string,
+    cssClass: PropTypes.string,
+    placeholder: PropTypes.string.isRequired,
+    callBackChange: PropTypes.func,
+    callBackFocus: PropTypes.func,
+    objCss: PropTypes.object,
+}
+
+InputBox.defaultProps = {
+    type: "text"
+  }  
+
+export default InputBox
