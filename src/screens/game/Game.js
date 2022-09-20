13afa498b6998dc9c6fcfd808a3cfacee7075ffa
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-20 16:47:13 +0200

    parallax

---
+++ src/screens/game/Game.js
@@ -0,0 +1,12 @@
+function Game() {
+  return (
+    <div className="bg-container">
+      <div className="layer layer-04"></div>
+      <div className="layer layer-03"></div>
+      <div className="layer layer-02"></div>
+      <div className="layer layer-01"></div>
+    </div>
+  )
+}
+
+export default Game
