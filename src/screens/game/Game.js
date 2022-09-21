fbf6784dd41d175ca1feb18b06afe21338874df7
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-21 12:47:31 +0200

    parallax

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -3,10 +3,11 @@ import './game.scss'
 function Game() {
   return (
     <div className="bg-container">
-      <div className="layer layer-04"> <div className='layer-04'></div></div>
-      <div className="layer layer-03"></div>
-      <div className="layer layer-02"></div>
-      <div className="layer layer-01"></div>
+      <div className="layer layer-04"><div className='bg bg-04'></div></div>
+      <div className="layer layer-03"><div className='bg bg-03'></div></div>
+      <div className="layer layer-02"><div className='bg bg-02'></div></div>
+      <div className="layer layer-01"><div className='bg bg-01'></div></div>
+      <div className='street'></div>
     </div>
   )
 }
