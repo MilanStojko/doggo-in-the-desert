44bb085101e146d8387af944da797f7e2393b5f3
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-21 14:37:43 +0200

    parallax che funziona

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -1,12 +1,16 @@
 import './game.scss'
 
 function Game() {
+
+  let viewportHeight = window.innerHeight
+  console.log(viewportHeight - 30)
+
   return (
     <div className="bg-container">
-      <div className="layer layer-04"><div className='bg bg-04'></div></div>
-      <div className="layer layer-03"><div className='bg bg-03'></div></div>
-      <div className="layer layer-02"><div className='bg bg-02'></div></div>
-      <div className="layer layer-01"><div className='bg bg-01'></div></div>
+      <div className="layer layer-04"></div>
+      <div className="layer layer-03"></div>
+      <div className="layer layer-02"></div>
+      <div className="layer layer-01"></div>
       <div className='street'></div>
     </div>
   )
