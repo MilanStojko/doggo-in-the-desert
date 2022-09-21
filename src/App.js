d30d15837bed83deed0bc528048c01d461a400ad
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-09-21 11:29:07 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import SCREENS from './route/router';
 import Home from './screens/home/Home';
 import Login from './screens/login/Login';
+import Tutorial from './screens/tutorial/Tutorial';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path={SCREENS.home} element={<Home />}/>
         <Route path={SCREENS.login} element={<Login />}/>
         <Route path={SCREENS.signup} element={<Login />}/>
-        <Route path={SCREENS.tutorial} element={<Login />}/>
+        <Route path={SCREENS.tutorial} element={<Tutorial />}/>
         <Route path={SCREENS.game} element={<Login />}/>
         <Route path={SCREENS.result} element={<Login />}/>
         <Route path={SCREENS.leaderboard} element={<Login />}/>
---
+++ src/screens/tutorial/Tutorial.js
@@ -0,0 +1,7 @@
+function Tutorial() {
+    return(
+        <div>Sono tutorial</div>
+    )
+}
+
+export default Tutorial
