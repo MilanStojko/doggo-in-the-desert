4053821a8a991d93b58e1e128da3a28bbefb0a01
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-09-23 14:52:10 +0200

    fixed landscape

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import logo from './logo.svg';
-import './app.scss';
+import logo from "./logo.svg";
+import "./app.scss";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import DeviceOrientation, { Orientation } from "react-screen-orientation";
 
-import SCREENS from './route/router';
-import Home from './screens/home/Home';
-import Game from './screens/game/Game';
-import Leaderboard from './screens/leaderboard/Leaderboard'
-import Login from './screens/login/Login';
-import Result from './screens/result/Result';
-import Signup from './screens/signup/Signup';
-import Tutorial from './screens/tutorial/Tutorial';
+import SCREENS from "./route/router";
+import Home from "./screens/home/Home";
+import Game from "./screens/game/Game";
+import Leaderboard from "./screens/leaderboard/Leaderboard";
+import Login from "./screens/login/Login";
+import Result from "./screens/result/Result";
+import Signup from "./screens/signup/Signup";
+import Tutorial from "./screens/tutorial/Tutorial";
 
 function App() {
-
   // screen.orientation.lock('landscape-primary')
 
   return (
     <div>
-      <Routes>
-        <Route path={SCREENS.home} element={<Home />}/>
-        <Route path={SCREENS.login} element={<Login />}/>
-        <Route path={SCREENS.signup} element={<Signup />}/>
-        <Route path={SCREENS.tutorial} element={<Tutorial />}/>
-        <Route path={SCREENS.game} element={<Game />}/>
-        <Route path={SCREENS.result} element={<Result />}/>
-        <Route path={SCREENS.leaderboard} element={<Leaderboard />}/>
-      </Routes>
+      <DeviceOrientation lockOrientation={"landscape"}>
+        <Orientation orientation="landscape" alwaysRender={false}>
+          <Routes>
+            <Route path={SCREENS.home} element={<Home />} />
+            <Route path={SCREENS.login} element={<Login />} />
+            <Route path={SCREENS.signup} element={<Signup />} />
+            <Route path={SCREENS.tutorial} element={<Tutorial />} />
+            <Route path={SCREENS.game} element={<Game />} />
+            <Route path={SCREENS.result} element={<Result />} />
+            <Route path={SCREENS.leaderboard} element={<Leaderboard />} />
+          </Routes>
+        </Orientation>
+        <Orientation orientation="portrait">
+          <div className="portrait-view">
+            <h1>Please rotate your device</h1>
+          </div>
+        </Orientation>
+      </DeviceOrientation>
     </div>
   );
 }
